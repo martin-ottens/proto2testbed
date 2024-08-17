664f6123fc0688d4bc8a9eeb4b6cac80737bc670
Martin Ottens <martin.ottens@fau.de>
2024-08-17 03:23:19 +0200

    Handle unescaped env variable values

--- scripts/variable_setter.sh
+++ scripts/variable_setter.sh
@@ -12,11 +12,12 @@ fi
 
 for var in $(env | cut -d '=' -f 1); do
     temp_file=$(mktemp)
+    value=$(printf '%s' "${!var}" | sed 's/[&/\]/\\&/g')
 
-    sed "s|{{${var}}}|${!var}|g" "$1" > "$temp_file"
+    sed "s|{{${var}}}|${value}|g" "$1" > "$temp_file"
 
     if ! cmp -s "$1" "$temp_file"; then
-        echo "Placeholder {{${var}}} replaced by ${!var}"
+        echo "Placeholder {{${var}}} replaced by ${value}"
         mv "$temp_file" "$1"
     else
         rm "$temp_file"
