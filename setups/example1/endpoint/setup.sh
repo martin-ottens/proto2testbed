909fa68cc829fb42c215d2ab5e9070befff553c1
Martin Ottens <martin.ottens@fau.de>
2024-09-02 05:07:22 +0200

    Add results

--- setups/example1/endpoint/setup.sh
+++ setups/example1/endpoint/setup.sh
@@ -7,7 +7,7 @@ ip r add $ROUTE
 if [[ "$WIREGUARD" != "disable" ]]; then
     ip link add dev wg0 type wireguard
     ip address add dev wg0 $WIREGUARD/24
-    curl $FILESERVER_ADDRESS/client/wireguard_${INSTANCE_NAME}.conf > /etc/wireguard/wg.conf
+    curl $FILESERVER_ADDRESS/endpoint/wireguard_${INSTANCE_NAME}.conf > /etc/wireguard/wg.conf
     wg setconf wg0 /etc/wireguard/wg.conf
     ip link set up dev wg0
 fi
