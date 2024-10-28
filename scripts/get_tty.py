5a0b6ee7c01bfe10b39dc0243bf844ad0231cde4
Martin <martin.ottens@fau.de>
2024-10-28 23:18:58 +0100

    Test: PTY to attach serial TTY

---
+++ scripts/get_tty.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import sys
+import pexpect
+
+def main(socket_path: str):
+    process = pexpect.spawn("/usr/bin/socat", [f"UNIX-CONNECT:{socket_path}", "STDIO,raw,echo=0"], 
+                            timeout=None, encoding="utf-8", echo=False)
+    process.send("\n")
+    process.readline()
+    process.interact()
+    process.terminate()
+    print("\n# Connection to serial TTY closed.")
+    if process.isalive():
+        print("scoat subprocess is still alive!")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage {sys.argv[0]} <vm-id>", file=sys.stderr)
+        sys.exit(1)
+
+    ids = sys.argv[1]
+    print(f"# Attached to Instance '{ids}', CRTL + ] to disconnect.")
+    main(f"/tmp/testbed-{ids}/tty.sock")
