c2fa047f272554cdf5288e7480ec7e5323a3dfc5
Martin <martin.ottens@fau.de>
2024-11-04 14:44:44 +0100

    get_tty: Show error when socket does not exist

--- scripts/get_tty.py
+++ scripts/get_tty.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import pexpect
 
 def main(socket_path: str):
@@ -20,5 +21,11 @@ if __name__ == "__main__":
         sys.exit(1)
 
     ids = sys.argv[1]
+
+    socket_path = f"/tmp/testbed-{ids}/tty.sock"
+    if not os.path.exists(socket_path):
+        print(f"# Instance with ID '{ids}' does not exist.")
+        sys.exit(1)
+
     print(f"# Attached to Instance '{ids}', CRTL + ] to disconnect.")
-    main(f"/tmp/testbed-{ids}/tty.sock")
+    main(socket_path)
