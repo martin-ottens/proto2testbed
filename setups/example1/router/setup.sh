f58c547c81778b1460e527cdbc26fad3c84e3eae
Martin Ottens <martin.ottens@fau.de>
2024-12-21 03:27:44 +0100

    Updates docs

--- setups/example1/router/setup.sh
+++ setups/example1/router/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ip address add $IP_ADDRESS_0 dev enp0s3
-ip link set up dev enp0s3
+ip address add $IP_ADDRESS_0 dev eth1
+ip link set up dev eth1
 
-ip address add $IP_ADDRESS_1 dev enp0s4
-ip link set up dev enp0s4
+ip address add $IP_ADDRESS_1 dev eth2
+ip link set up dev eth2
 
 sysctl -w net.ipv4.ip_forward=1
